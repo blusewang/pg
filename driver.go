caa80a0d4f3ff5146cf7a1f706c31462e93941c7
jf.wang <dev.jf.wang@gmail.com>
2019-01-11 00:08:13 +0800

    connect

--- driver.go
+++ driver.go
@@ -8,19 +8,12 @@ package pg
 
 import (
 	"database/sql/driver"
-	"fmt"
 	"github.com/blusewang/pg/internal/conn"
 )
 
 type Driver struct{}
 
 func (d *Driver) Open(name string) (_ driver.Conn, err error) {
-	//
-	defer func(err *error) {
-		if e := recover(); e != nil {
-			*err = fmt.Errorf("pg: Open Error: %#v", e)
-		}
-	}(&err)
 
 	c, err := conn.Open(name)
 
