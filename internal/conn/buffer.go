c382d8299d8e0fdcb4eb9d0764773d1e333c6288
jf.wang <dev.jf.wang@gmail.com>
2019-01-13 09:36:27 +0800

    buffer 设计

---
+++ internal/conn/buffer.go
@@ -0,0 +1,7 @@
+// Copyright 2019 MQ, Inc. All rights reserved.
+//
+// Use of this source code is governed by a MIT license
+// that can be found in the LICENSE file in the root of the source
+// tree.
+
+package conn
